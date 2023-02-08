281d421d64bb7569440ee3f181682864c822d155
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2023-02-08 15:52:57 +0300

    chore: check affected projects

--- apps/mfe1/src/app/app.component.ts
+++ apps/mfe1/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
 	selector: 'angular15mf-root',
-	template: '<router-outlet></router-outlet>',
+	template: '1<router-outlet></router-outlet>',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
