9a81edd0bac4a24274068198549d07e916c6af79
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2023-03-05 17:35:21 +0300

    chore: add build and push

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,6 +1,9 @@
 import { loadMfeManifest } from './utils/load-mfe-manifest.util';
+import { isDevMode } from '@angular/core';
 
-loadMfeManifest()
+loadMfeManifest(
+	isDevMode() ? 'assets/mf.manifest.json' : 'assets/mf.manifest.prod.json'
+)
 	.catch((err) => console.error(err))
 	.then(() => import('./bootstrap'))
 	.catch((err) => console.error(err));
