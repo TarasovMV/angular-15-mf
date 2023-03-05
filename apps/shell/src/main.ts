f81b893cfcfdf3b409518ee72daab2b8de8c4194
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2023-03-05 18:14:35 +0300

    fix: import

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,9 +1,6 @@
 import { loadMfeManifest } from './utils/load-mfe-manifest.util';
-import { isDevMode } from '@angular/core';
 
-loadMfeManifest(
-	isDevMode() ? 'assets/mf.manifest.json' : 'assets/mf.manifest.prod.json'
-)
+loadMfeManifest()
 	.catch((err) => console.error(err))
 	.then(() => import('./bootstrap'))
 	.catch((err) => console.error(err));
