13a29efd66d81f8bb4a2881bfd371bc984fb6649
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2023-04-01 15:26:18 +0300

    chore: replace config to cdn

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,6 +1,6 @@
 import { loadMfeManifest } from './utils/load-mfe-manifest.util';
 
-loadMfeManifest('assets/mf.manifest.prod.json')
+loadMfeManifest('https://cdn.m-tarasov.com/mf-demo/mf.manifest.json')
 	.catch((err) => console.error(err))
 	.then(() => import('./bootstrap'))
 	.catch((err) => console.error(err));
