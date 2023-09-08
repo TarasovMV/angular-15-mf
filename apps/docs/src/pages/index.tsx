3bf950bbc865352940a5184b97ffbff0809063d3
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2023-09-08 10:22:42 +0300

    docs: update

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -35,9 +35,7 @@ export default function Home(): JSX.Element {
 			description="Description will go into a meta tag in <head />"
 		>
 			<HomepageHeader />
-			<main>
-				<HomepageFeatures />
-			</main>
+			<main>{/*<HomepageFeatures />*/}</main>
 		</Layout>
 	);
 }
